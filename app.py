50b34442e477af7c72c02a740293511d64c4c814
regibiy <19412550_regi_r_b@widyadharma.ac.id>
2025-11-08 10:05:19 +0700

    menyelesaikan input form wo dan sudah tampil baik di ppt. lanjut input form unit

--- app.py
+++ app.py
@@ -22,34 +22,60 @@ def home():
 def generate():
     caseNumber = request.form["caseNumber"]
     wo = request.files["WO"]
-    return wo
-    # wo = request.files["WO"]
-    # prs = Presentation("template.pptx")
-    # slide = prs.slides[0]
-    # table = slide.shapes[0].table
-    # table.cell(0,1).text = caseNumber
 
+    # photoUnitList = request.files.getlist("addPhoto[]")
+    # for i, photo in enumerate(photoUnitList, start=1):
+    #     if photo and photo.filename != "":
+    #          print(f"Gambar ke-{i}: {photo.filename}")
+    # return "OK"
+    
+    # open template and set which slide
+    prs = Presentation("template.pptx")
+    slide = prs.slides[0]
+
+    # for i, shape in enumerate(slide.shapes):
+    #     print(i, "-", shape.name, "-", shape.shape_type)
+
+    # return "OK"
+    
+    # determine the first shape
+    table = slide.shapes[0].table
+    table.cell(0,1).text = caseNumber
+
+    # determine the second shape
+    shape = slide.shapes[1]
+
+    # begin get position and size shape
+    left = shape.left + Inches(0.03)
+    top = shape.top + Inches(0.03)
+    width = shape.width - Inches(0.05)
+    height = shape.height - Inches(0.05)
+    # end begin get position and size shape
+    
+    slide.shapes.add_picture(wo, left, top, width, height)
+
+    # begin insert image
     # with NamedTemporaryFile(delete=False, suffix=".jpeg") as img_temp:
     #     wo.save(img_temp.name)
 
     # slide.shapes.add_picture(img_temp.name, Inches(1), Inches(3), width=Inches(3), height=Inches(2))
+    # end insert image
 
-    # tmp = NamedTemporaryFile(delete=False, suffix=".pptx")
-    # prs.save(tmp.name)
+    tmp = NamedTemporaryFile(delete=False, suffix=".pptx")
+    prs.save(tmp.name)
 
-    # return send_file(
-    #     tmp.name,
-    #     as_attachment=True,
-    #     download_name = f"{caseNumber}.pptx",
-    #     mimetype="application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    # )
+    return send_file(
+        tmp.name,
+        as_attachment=True,
+        download_name = f"{caseNumber}.pptx",
+        mimetype="application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    )
 
-    # return f"<h1>{caseNumber}</h1>"
-
-#mengambil nilai apapun dari url ke dalam web
+# begin mengambil nilai apapun dari url ke dalam web
 @app.route("/<name>")  
 def user(name):
     return f"hello {name}!"
+# end begin mengambil nilai apapun dari url ke dalam web
 
 @app.route("/admin")
 def admin():
