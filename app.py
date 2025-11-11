e6272d7155608cd93d68a0371fb06b3e05401248
regibiy <19412550_regi_r_b@widyadharma.ac.id>
2025-11-11 21:34:43 +0700

    layout foto sudah. tinggal atur shape inputan mekanik, set text from mekanik, set image from mekanik

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import sys
 from flask import Flask, redirect, url_for, render_template, request, send_file
 from io import BytesIO
 from pptx import Presentation
 from pptx.util import Inches
 from PIL import ImageFont
 from pptx.enum.text import PP_ALIGN
+from pptx.enum.shapes import MSO_SHAPE
 
 app = Flask(__name__)
 
@@ -15,56 +17,21 @@ def home():
 # def about():
 #     return render_template("about.html")
 
+# begin set table transparent
+# for row in new.rows:
+#     for cell in row.cells:
+#         cell.fill.background()
+# end set table transparent 
+
 @app.route("/generate", methods=["POST"])
 def generate():
-    # need to count unit photo to determine how many table will be created
-    caseNumber = request.form["caseNumber"]
-
-    prs = Presentation("template.pptx")
-    SLD_LAYOUT_TITLE_AND_CONTENT = 6
-    slideLayout = prs.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT]
-    slide = prs.slides.add_slide(slideLayout)
-
-    newTable = slide.shapes.add_table(2, 2, Inches(0.5), Inches(0.5), prs.slide_width - (2 * Inches(0.5)), Inches(1)) # add elif one unit or not
-    new = newTable.table
-
-    test = new.cell(0, 0)
-    test.text = "Ini baris 1 kolom 1"
-    new.cell(0, 1).text = "Ini baris 1 kolom 2"
-    paragraph = test.text_frame.paragraphs[0]
-    paragraph.alignment = PP_ALIGN.CENTER    
-
-    
-    # for row in new.rows:
-    #     for cell in row.cells:
-    #         cell.fill.background() 
-
-    pptStream = BytesIO()
-    prs.save(pptStream)
-    pptStream.seek(0)
-
-    return send_file(
-        pptStream,
-        as_attachment=True,
-        download_name = f"{caseNumber}.pptx",
-        mimetype="application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    )
-
-
-@app.route("/generate2", methods=["POST"])
-def generate2():
     def setPosSiShape(shape):
         # begin get position and size shape
         left = shape.left + Inches(0.03)
         top = shape.top + Inches(0.03)
         width = shape.width - Inches(0.05)
         height = shape.height - Inches(0.05)
-        posSiShape = {
-            "left" : left,
-            "top" : top,
-            "width" : width,
-            "height" : height
-        }
+        posSiShape = {"left" : left, "top" : top, "width" : width, "height" : height}
         return posSiShape
         # end begin get position and size shape
     
@@ -95,8 +62,17 @@ def generate2():
     # begin get all unit photo 
     photoUnitList = request.files.getlist("addPhoto[]")
     infoUnitList = request.form.getlist("addInfo[]")
+    SLD_LAYOUT_TITLE_AND_CONTENT = 6
+    slideLayout = prs.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT]
+    row = 1
+    slide2 = None
+    tempIndex = 0
+    slideWidth = 0
+    currentIndex = 0
+    rectangleShape = None
+
     for i, (photo, info) in enumerate(zip(photoUnitList, infoUnitList), start=1):
-        if photo and photo.filename != "":
+        # if photo and photo.filename != "":
             if i == 1:
                 photoStream = BytesIO(photo.read())
                 getPosSiShape2 = setPosSiShape(slide.shapes[2])
@@ -107,16 +83,50 @@ def generate2():
                 spTree = slide.shapes._spTree
                 spTree.remove(slide.shapes[3]._element)
 
-                font = ImageFont.truetype("arial.ttf", 18)
+                font = ImageFont.truetype("calibri.ttf", 18)
                 bbox = font.getbbox(info)
                 textWidth = (bbox[2] - bbox[0]) / 96
                 newLeft = oldPosInfo["left"] + (oldPosInfo["width"] - Inches(textWidth)) / 2
-                
+                    
                 newShapeInfo = slide.shapes.add_shape(shape.auto_shape_type, newLeft, oldPosInfo["top"], Inches(textWidth), oldPosInfo["height"])
                 newShapeInfo.text = info
-            else:
-                prs.slides.add_slide(prs.slide_layouts[1])
 
+            if i >= 2 and (i - 2) % 4 == 0: 
+                slide2 = prs.slides.add_slide(slideLayout)
+                slideWidth = prs.slide_width
+                currentIndex = len(prs.slides)
+
+            if (currentIndex != tempIndex):
+                row = 1
+
+            tempIndex = currentIndex
+
+            cols = 2
+            gapY = Inches(1.25)
+            margin = Inches(0.3)
+            height = Inches(0.5)
+            shapeWidth = (slideWidth - (3 * margin)) / 2
+
+            if slide2 is not None:
+                row += 1
+                col = i % cols
+                row2 = row // cols
+                x = margin + col * (shapeWidth + margin)
+                if row2 == 1:
+                    y = Inches(0.2)
+                else:
+                    y = margin + row2 * (height + gapY)
+                rectangleShape = slide2.shapes.add_shape(MSO_SHAPE.RECTANGLE, x, y, shapeWidth, height)
+
+            if rectangleShape is not None:
+                rectangleShape.text = "Ini contoh keterangan yang akan diberikan oleh mekanik"
+                testLeft = rectangleShape.left
+                testTop = rectangleShape.top
+                widthCol0 = Inches(3)
+                heightRow0 = rectangleShape.height
+
+            if slide2 is not None:
+                slide2.shapes.add_picture("meneketehek.png", testLeft, testTop + heightRow0, widthCol0, Inches(3))
     # end get all unit photo
 
     pptStream = BytesIO()
@@ -130,15 +140,15 @@ def generate2():
         mimetype="application/vnd.openxmlformats-officedocument.presentationml.presentation"
     )
 
-# begin mengambil nilai apapun dari url ke dalam web
-@app.route("/<name>")  
-def user(name):
-    return f"hello {name}!"
-# end begin mengambil nilai apapun dari url ke dalam web
+# # begin mengambil nilai apapun dari url ke dalam web
+# @app.route("/<name>")  
+# def user(name):
+#     return f"hello {name}!"
+# # end begin mengambil nilai apapun dari url ke dalam web
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name = "Dyah")) #hanya redirect halaman biasa
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user", name = "Dyah")) #hanya redirect halaman biasa
 
 if __name__ == "__main__":
     app.run(debug=True) 
